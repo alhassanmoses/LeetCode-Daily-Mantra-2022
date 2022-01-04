47c1fe2c3e9b5c738fd8762c7a1d7c3cbf87fd39
Moses Wuniche Alhassan <Misty@MACs-MacBook-Pro.local>
2022-01-04 20:59:23 +0000

    addded solution for challenge 743

---
+++ Fizz Buzz.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/743/
+
+from typing import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        
+        result = []
+        for i in range(1, n + 1):
+            
+            if (i % 3 == 0) and (i % 5 == 0):
+                
+                result.append('FizzBuzz')
+                
+            elif (i % 3 == 0):
+                
+                result.append('Fizz')
+               
+            elif (i % 5 == 0):
+                
+                result.append('Buzz')
+                
+            else:
+                
+                result.append(str(i))
+                
+        return result
+
+if __name__ == "__main__":
+    
+    sol = Solution()
+    n = 15
+    print(sol.fizzBuzz(n))
